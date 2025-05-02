f5e40649832b303c487269ce99ae181e656dd4dd
Martin Forbelský <mforbelak@gmail.com>
2025-05-02 12:22:54 +0200

    použití user routeru

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+//import routerů
 import homeRouter from './routers/home_router.js';
+import userRouter from './routers/user_router.js';
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -9,12 +11,12 @@ const __dirname = path.dirname(__filename);
 
   
 
-// Sending json in requests
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../www')));
 
 
 app.use('/', homeRouter());
+app.use('/api', userRouter());
 
 
 
